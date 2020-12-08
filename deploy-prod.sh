717464732a43c979b699036b5879dea32cb90ae3
BenjiFTL <74981463+BenjiFTL@users.noreply.github.com>
2020-12-08 12:35:02 -0500

    Update deploy-prod.sh

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,5 +1,5 @@
 
-PROJECT_ID=esp8266-ota-update
+PROJECT_ID=YOUR_GCLOUD_PEOJCT_NAME
 gcloud config set project $PROJECT_ID
 gcloud functions deploy insertFirmwaresOnBigquery --runtime nodejs8 --trigger-resource $PROJECT_ID-firmwares --trigger-event google.storage.object.finalize
 gcloud functions deploy getDownloadUrl --runtime nodejs8 --trigger-http
